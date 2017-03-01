081bd200d11484b3dba222511b4f16696a1536f3
Trung Pham Hieu <popeveryday@Trungs-MacBook-Air.local>
2017-03-01 16:09:29 +0700

    0.2.02

--- Example/Pods/Target Support Files/Pods-POPLib_Example/Pods-POPLib_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-POPLib_Example/Pods-POPLib_Example-frameworks.sh
@@ -90,10 +90,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/POPLib/POPLib.framework"
   install_framework "$BUILT_PRODUCTS_DIR/PureLayout/PureLayout.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/POPLib/POPLib.framework"
   install_framework "$BUILT_PRODUCTS_DIR/PureLayout/PureLayout.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
